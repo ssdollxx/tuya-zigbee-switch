90ea6c24c2e69fc310daad0105d73d840c8bcb13
Roman Skurikhin <romasku135@gmail.com>
2025-01-18 19:56:17 +0200

    Fix ts0001, ts0002_2 image type

--- zigbee2mqtt/converters/tuya_with_ota.js
+++ zigbee2mqtt/converters/tuya_with_ota.js
@@ -2,10 +2,10 @@ const tuyaDefinitions = require("zigbee-herdsman-converters/devices/tuya");
 
 const tuyaModels = [
     "WHD02",
+    "TS0002_basic",
+    "TS0001_switch_module",
     "TS0012_switch_module",
     "TS0011_switch_module",
-    "TS0001_switch_module",
-    "TS0002_basic",
 ];
 
 const definitions = [];
