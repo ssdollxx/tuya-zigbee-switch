221e1c67b975d824981b833bc5d3fbbf79bee6ba
clumsy-stefan <32835100+clumsy-stefan@users.noreply.github.com>
2025-07-28 16:28:50 +0200

    Fix OTA for 4-Gang switches
    When the config includes 4 switches and 4 relays OTA is not possible anymore due to not enough clusters (clusterId 25 is somehow overwritten).
    Can be reproduced with any 3- or 4-Gang module, just enhance the config string.

--- src/configs/stack_cfg.h
+++ src/configs/stack_cfg.h
@@ -49,7 +49,7 @@
  *  @brief  ZCL: MAX number of cluster list, in cluster number add  + out cluster number
  *
  */
-#define ZCL_CLUSTER_NUM_MAX        20
+#define ZCL_CLUSTER_NUM_MAX        32
 
 /**
  *  @brief  ZCL: maximum number for zcl reporting table
