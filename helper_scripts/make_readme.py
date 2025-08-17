af2991ed0bfa923999b2baec45e7d0c0103416af
Andrei LAZAROV <andrei_lazarov@yahoo.com>
2025-08-18 00:47:13 +0300

    Split tuya_manufacturer_names
    into tuya_manufacturer_name and old_manufacturer_names; adjust scripts

--- helper_scripts/make_readme.py
+++ helper_scripts/make_readme.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     by_manufacturer_names = {}
 
     for device in devices:
-        manufacturer_name = next(name for name in device["tuya_manufacturer_names"] if name.startswith("_TZ3000"))
+        manufacturer_name = device["tuya_manufacturer_name"]
         if manufacturer_name not in by_manufacturer_names:
             by_manufacturer_names[manufacturer_name] = {
                 **device,
